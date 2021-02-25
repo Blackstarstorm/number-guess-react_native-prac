1674ba8ae198e8598cb6c5c1433bf7be268629a6
Winston Meikle <winmeikle@gmail.com>
2021-02-25 15:16:19 -0500

    learned more

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,5 +1,7 @@
-import React, { useState }from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import React, { useState, useRef }from "react";
+import { View, StyleSheet, Text, Button, Alert } from "react-native";
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card"
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
@@ -16,8 +18,47 @@ const GameScreen = props => {
   const [currentGuess, setCurrentGuess] = useState(generateRandomBetween(1, 100, props.userChoice));
 };
 
-const styles = StyleSheet.create({
+const currrentLow = useRef(1);
+const currrentHigh = useRef(100);
 
+const nextGuessHandler = direction => {
+  if ((direction === "lower" && currentGuess < props.userChoice) || (direction === "greater" && currentGuess > props.userChoice)) {
+    Alert.alert("Play Fair Now", "You know you're being dishonest... ", [{ text: "My Bad", style: "cancel" }]);
+    return;
+  } 
+  if (direction === "lower") {
+    currrentHigh.current = currentGuess;
+  } else {
+    currentLow.current = currentGuess
+  }
+
+  const nextNumber = generateRandomBetween(currrentLow.current, currrentHigh.current, setCurrentGuess(nextNumber));
+};
+
+return (
+  <View styles={style.screen}>
+    <Text>Oppenent's Guess:</Text>
+    <NumberContainer>{currentGuess}</NumberContainer>
+    <Card styles={style.buttonContainer}>
+      <Button title="LOWER" onPress={(nextGuessHandler.bind(this, "lower"))}/>
+      <Button title="GREATER" onPress={(nextGuessHandler.bind(this, "greater"))}/>
+    </Card>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 300,
+    maxWidth: "80%"
+  }
 })
 
 export default GameScreen;
